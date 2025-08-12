b1927e570ecc5a88e239e449f46ff093182688b0
AlexKolbe <helping5@yandex.ru>
2025-08-12 17:33:36 +0300

    добавили описание работы кода

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -15,7 +15,21 @@
     Подключите модуль в проект.
 */
 
+/* Ищем в DOM элемент с id="picture" (шаблон <template>)
+      получаем ссылку на элемент-шаблон
+    .content:
+        Обращается к специальному свойству элемента <template>
+        Возвращает DocumentFragment с содержимым шаблона
+        Это "виртуальный контейнер" с HTML-структурой внутри шаблона
+    Результат:
+        pictureTemplate теперь содержит "заготовку" для будущих миниатюр
+        Это не DOM-элемент, а специальный объект, который можно клонировать
+*/
 const pictureTemplate = document.querySelector('#picture').content;
+
+/*  Ищем в DOM первый элемент с классом pictures
+    container становится точкой вставки для будущих миниатюр
+*/
 const container = document.querySelector('.pictures');
 
 
@@ -29,17 +43,28 @@ const container = document.querySelector('.pictures');
  * @param {number} photo.likes - количество лайков
  * @param {Array} photo.comments - массив комментариев
  * @returns {DocumentFragment} - клонированный шаблон с заполненными данными
+ *
+ *   <!-- Шаблон изображения случайного пользователя -->
+  <template id="picture">
+    <a href="#" class="picture">
+      <img class="picture__img" src="" width="182" height="182" alt="Случайная фотография">
+      <p class="picture__info">
+        <span class="picture__comments"></span>
+        <span class="picture__likes"></span>
+      </p>
+    </a>
+  </template>
  */
 const createThumbnail = ({id, url, description, likes, comments}) => {
-  const pictureElement = pictureTemplate.cloneNode(true);
-  const picture = pictureElement.querySelector('.picture');
+  const pictureElement = pictureTemplate.cloneNode(true); // Полная копия DOM узла
+  const picture = pictureElement.querySelector('.picture'); // Первый элемент из шаблона
   const pictureImg = pictureElement.querySelector('.picture__img');
 
   picture.dataset.photoId = id; // Лучше использовать data-атрибут для ID
-  pictureImg.src = url;
-  pictureImg.alt = description;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureImg.src = url; // URL изображения
+  pictureImg.alt = description; // атрибут alt изображения
+  pictureElement.querySelector('.picture__likes').textContent = likes; // количество лайков
+  pictureElement.querySelector('.picture__comments').textContent = comments.length; // количество комментариев
 
   return pictureElement;
 };
@@ -83,13 +108,17 @@ const createThumbnail = ({id, url, description, likes, comments}) => {
  */
 const renderThumbnails = (pictures) => {
 
-  const fragment = document.createDocumentFragment();
+  const fragment = document.createDocumentFragment(); //Создаёт "виртуальный контейнер" (DocumentFragment) в памяти. Временная коробка.
 
-  pictures.forEach((photo) => {
-    fragment.appendChild(createThumbnail(photo));
+  pictures.forEach((photo) => { // Перебор фотографий по каждому элементу массива photo. Т.е. для каждой фотографии будем выполнять
+    fragment.appendChild(createThumbnail(photo)); //Создание и добавление миниатюры.
+    /*
+      createThumbnail(photo) - создаёт DOM-элемент миниатюры на основе данных
+      fragment.appendChild() - добавляет созданную миниатюру в "виртуальный контейнер". Т.е. кладём в коробку.
+    */
   });
 
-  container.appendChild(fragment);
+  container.appendChild(fragment); // container - существующий DOM-элемент (получен через querySelector ранее). Добавляет все собранные миниатюры из фрагмента в реальный DOM одной операцией. Т.е. всё содержимое коробки переместили в DOM
 };
 
 export { renderThumbnails };
