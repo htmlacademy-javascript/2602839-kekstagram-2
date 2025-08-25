7a425b739740a0c48b2f42268c55e96f4579732c
AlexKolbe <helping5@yandex.ru>
2025-08-25 11:30:28 +0300

    причесали код и комментарии в нём

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,21 +1,10 @@
 import { EscKey } from './utils.js';
-/*
-Задача
 
-Каждый объект с описанием фотографии содержит массив с комментариями. Данные из этого массива мы вывели в соответствующую область окна полноразмерного просмотра. Все бы хорошо, но для популярных фотографий комментариев может быть много. Если вывести их разом, то пользователю будет неудобно взаимодействовать с окном просмотра. Улучшить пользовательский интерфейс поможет кнопка «Загрузить ещё».
-
-    Покажите блоки счётчика комментариев .social__comment-count и загрузки новых комментариев .comments-loader, убрав у них класс hidden.
-
-    В модуле, который отвечает за отрисовку окна с полноразмерным изображением, доработайте код по выводу списка комментариев таким образом, чтобы список показывался не полностью, а по 5 элементов, и следующие 5 элементов добавлялись бы по нажатию на кнопку «Загрузить ещё». Не забудьте реализовать обновление числа показанных комментариев в блоке .social__comment-count.
-
-    Обратите внимание, хотя кнопка называется «Загрузить ещё», никакой загрузки с сервера не происходит. Просто показываются следующие 5 комментариев из списка.
-
-
-*/
-
-
+// Константы
 const COMMENTS_PER_PORTION = 5; // порция комментов по сколько будем выводить
 
+
+// DOM элементы
 const fullSizePhoto = document.querySelector('.big-picture');//Большое всплывающее окно
 const closeButton = fullSizePhoto.querySelector('.big-picture__cancel'); // кнопка закрытия
 const commentsList = fullSizePhoto.querySelector('.social__comments'); // список комметариев
@@ -26,15 +15,13 @@ const socialCommentTotalCount = fullSizePhoto.querySelector('.social__comment-to
 const socialCommentCount = fullSizePhoto.querySelector('.social__comment-count'); // место показывания кол-ва коммментов на модалке
 const commentsLoader = fullSizePhoto.querySelector('.comments-loader'); // кнопка "Загрузить ещё". Понадобиться для показывания порций комментов.
 
-
+// Состояние приложения
 let comments = []; // переменная для комментариев в этом модуле
 let commentsShown = 0; // счётчик комментов для модалки
 
 /**
- * функция закрытия картинок
- * добавляем класс .hidden обычным фото
- * убираем класс overflow-hidden
- *
+ * Закрывает модальное окно с полноразмерным изображением
+ * Восстанавливает скролл страницы и удаляет обработчики событий
  */
 
 const closeBigPic = () => {
@@ -47,8 +34,8 @@ const closeBigPic = () => {
 };
 
 /**
- * Функция закрытия большой картинки при нажатии Esc
- * @param {Event} evt - событие нажатие кнопки Esc
+ * Обработчик нажатия клавиши Esc для закрытия модального окна
+ * @param {Event} evt - Событие клавиатуры
  */
 function onDocumentKeydown(evt) {
   if (EscKey(evt)) {
@@ -58,29 +45,33 @@ function onDocumentKeydown(evt) {
 }
 
 /**
- * Функция закрытия закрытия модального окна
+ * Обработчик клика по кнопке закрытия модального окна
  */
 function onCloseButtonClick() {
   closeBigPic();
 }
 
-/*
-        Список комментариев под фотографией: комментарии должны вставляться в блок .social__comments. Разметка каждого комментария должна выглядеть так:
-
-        <li class="social__comment">
-          <img
-            class="social__picture"
-            src="{{аватар}}"
-            alt="{{имя комментатора}}"
-            width="35" height="35">
-          <p class="social__text">{{текст комментария}}</p>
-        </li>
-*/
-
 /**
- * Функция создания одного комментария
- * @param {object}
- * @returns {object}
+ * Создает DOM-элемент комментария на основе данных
+ * @param {Object} commentData - Данные комментария
+ * @param {string} commentData.avatar - URL аватара пользователя
+ * @param {string} commentData.name - Имя пользователя
+ * @param {string} commentData.message - Текст комментария
+ * @returns {HTMLElement} Элемент комментария
+ *
+ * @example
+ * // Возвращает такой HTML:
+ * // <li class="social__comment">
+ * //   <img class="social__picture" src="avatar.jpg" alt="John Doe" width="35" height="35">
+ * //   <p class="social__text">Отличная фотография!</p>
+ * // </li>
+ *
+ * @example
+ * createComment({
+ *   avatar: 'avatar.jpg',
+ *   name: 'John Doe',
+ *   message: 'Отличная фотография!'
+ * });
 */
 const createComment = ({ avatar, name, message }) => {
   const comment = commentItem.cloneNode(true);
@@ -94,93 +85,102 @@ const createComment = ({ avatar, name, message }) => {
 
 
 /**
- * Функция создания-отрисовки массива-списка комментариев
- *
- */
+ * Рендерит порцию комментариев с учетом текущего состояния
+ * Управляет видимостью кнопки "Загрузить ещё" и счетчиками
+*/
 const renderComments = () => {
-
-  // Надо что-то подумать с кнопкой, когда её показывать, а когда не надо показывать
-
-  // показанные комменты количество
   commentsShown += COMMENTS_PER_PORTION;
 
-  // проверяем сколько кол-во показанных больше или равно длине массива комментов
+  // Управление видимостью кнопки загрузки
   if (commentsShown >= comments.length) { // если количество показаннх больше чем длина ммассива комментов, то например длина массива комментов 3, а порция 5
     commentsLoader.classList.add('hidden'); // прячем кнопку "Загрузить ещё"
-    commentsShown = comments.length; // количесов показанных равно длине массива комментов.
+    commentsShown = comments.length; // количество показанных равно длине массива комментов
   } else {
     commentsLoader.classList.remove('hidden'); // иначе убираем скрытие кнопки "Загрузить ещё"
   }
 
+  // Создание фрагмента с комментариями
   const commentsFragment = document.createDocumentFragment();
-  commentsList.innerHTML = '';
+
+  // Обновление DOM
   for (let i = 0; i < commentsShown; i++) { // теперь перебираем до длины кол-ва показанных
-    const comment = createComment(comments[i]); // вызываем функцию создания комментов
+    const comment = createComment(comments[i]); // вызываем функцию создания коммента
     commentsFragment.append(comment); // дообавляем коммент в конец списка комментов
   }
 
+  commentsList.innerHTML = ''; // удаляем все комментарии что были ранее выведены
   commentsList.append(commentsFragment); // добавляем единовременно весь список комментов
 
-  // надо присваивать значения. Сколько мы показываем и сколько всего комментов.
-  // как мы поймём что нужно выводить-показывать новые комменты взамен старых ?
-
+  // Обновление счетчиков
   socialCommentShownCount.textContent = commentsShown; // сколько показываем
   socialCommentTotalCount.textContent = comments.length; // общая длина массива комментов для этой фото
 
 };
 
-/**Функция по отрисовки карточки при открытии в модалке
- *
- * @param {string} url - ссылка на фотографию
- * @param {string} description - описание фотографии
- * @param {int} likes - количество лайков
+/**
+ * Рендерит информацию о фотографии в модальном окне
+ * @param {Object} photoData - Данные фотографии
+ * @param {string} photoData.url - URL изображения
+ * @param {number} photoData.likes - Количество лайков
+ * @param {string} photoData.description - Описание фотографии
   */
 
 const renderPictureInformation = ({url, likes, description}) => {
-  fullSizePhoto.querySelector('.big-picture__img img').src = url;
-  fullSizePhoto.querySelector('.big-picture__img img').alt = description;
+  const imgElement = fullSizePhoto.querySelector('.big-picture__img img'); // облегчаем читаемость
+
+  imgElement.src = url;
+  imgElement.alt = description;
   fullSizePhoto.querySelector('.likes-count').textContent = likes;
   fullSizePhoto.querySelector('.social__caption').textContent = description;
 };
 
-/** Функция добавления комментов в список
- *
- * @param {*} dataComments
+/**
+ * Обработчик клика по кнопке "Загрузить ещё"
+ * Загружает следующую порцию комментариев и обновляет счетчик
  */
 
-const onCommentsLoadClick = (dataComments) => {
-  renderComments(dataComments);
-  socialCommentCount.textContent = `${socialCommentShownCount.textContent} из ${socialCommentTotalCount.textContent} комментариев`; // формирование вывода по кол-ву комментов
+/**
+ * Обновляет текстовое представление счетчика комментариев
+ */
+const updateCommentsCounter = () => {
+  socialCommentCount.textContent =
+    `${socialCommentShownCount.textContent} из ${socialCommentTotalCount.textContent} комментариев`;
+};
+
+/**
+ * Обработчик клика по кнопке "Загрузить ещё"
+ * Загружает следующую порцию комментариев и обновляет счетчик
+ */
+const onCommentsLoadClick = () => {
+  renderComments();
+  updateCommentsCounter();
 };
 
 
 /**
- * Отрисовка модального окошка. Работаем с классами. Добавляем и убираем.
- * @param {int} id - идентификатор фотографии
- * @param {Array} comments - массив комментариев делали в data.js
+ * Открывает модальное окно с полноразмерным изображением и комментариями
+ * @param {Object} data - Данные для отображения
+ * @param {Array} data.comments - Массив комментариев
  */
 const openBigPic = (data) => {
-  // commentsList.innerHTML = '';
   comments = data.comments;
+
+  // Настройка модального окна
   fullSizePhoto.classList.remove('hidden'); // убираем у большой фото класс hidden
   document.body.classList.add('modal-open'); // добавляем для body класс модалка открыта
 
-  // socialCommentCount.classList.add('hidden'); // прячем данные  по количеству комментов
-  // commentsLoader.classList.add('hidden'); // ПОКА прячем кнопку загрузки комментов
-
-
+  // Добавление обработчиков событий
   document.addEventListener('keydown', onDocumentKeydown); // ожидаем нажатия кнопки
   closeButton.addEventListener('click', onCloseButtonClick); // ожидаем клика по кнопке закрытия модалки
 
+  // Рендер контента
   renderPictureInformation(data); // рендер картинок перенести в отельную фнкцию
-  renderComments(data.comments); // рендер комментариев перенести в отджельную функцию
-
-  socialCommentCount.textContent = `${socialCommentShownCount.textContent} из ${socialCommentTotalCount.textContent} комментариев`; // формирование вывода по кол-ву комментов
+  renderComments(data.comments); // рендер комментариев перенести в отдельную функцию
+  updateCommentsCounter(); // формирование вывода по кол-ву комментов
 };
 
-// не работает загрузка по кнопке "Загрузить ещё" - надо исправлять
 
-//Ждём click для запуска отрисовки
+// Обработчик кнопки загрузки комментариев
 commentsLoader.addEventListener('click', () => {
   onCommentsLoadClick(comments);
 });
