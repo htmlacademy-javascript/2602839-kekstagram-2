58d89a5bdbb8dcc940b81d73c472d5d81db08e46
AlexKolbe <helping5@yandex.ru>
2025-08-27 17:12:24 +0300

    создали файл в котором будем писать проверку форм. Подключили скрапт для  Pristine. Импортировали в main Ставим себе задачу.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { getAllPhotoUsers } from './data.js';
 import { renderThumbnails } from './thumbnails.js';
+import './form.js';
 
 //генерируем миниатюры с данными юзеров
 const photos = getAllPhotoUsers();
