073420a88849d87988d7be4c8aa87fb3b9d40eac
AlexKolbe <helping5@yandex.ru>
2025-09-11 15:22:05 +0300

    Сделали проверку форм. Ошибка должна выводится красным

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { getAllPhotoUsers } from './data.js';
 import { renderThumbnails } from './thumbnails.js';
 import './form.js';
+import './form-validator.js';
 
 //генерируем миниатюры с данными юзеров
 const photos = getAllPhotoUsers();
