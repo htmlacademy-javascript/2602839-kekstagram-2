5b8b3a755171c04d807858c6ed1ffba8dd00834d
AlexKolbe <helping5@yandex.ru>
2025-09-15 14:44:19 +0300

    Запустили запрос фетч

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 import { getAllPhotoUsers } from './data.js';
 import { renderThumbnails } from './thumbnails.js';
 import './form.js';
-
+import { getData } from './api.js';
 
 //генерируем миниатюры с данными юзеров
 const photos = getAllPhotoUsers();
 renderThumbnails(photos);
+getData();
