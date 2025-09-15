20f833dd001a26ce1d03e88d5ed582f1499bb961
AlexKolbe <helping5@yandex.ru>
2025-09-15 17:22:05 +0300

    На сервер отправляет, но через АКШЕН, но пока не работает без этого НЕ выводит ещё сообщение по ошибке загрузки фото с сервера, или не могу поймать.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 //import { getAllPhotoUsers } from './data.js';
 import { renderThumbnails } from './thumbnails.js';
 import './form.js';
-import { data, getData } from './api.js';
+import { data } from './api.js';
+import './form-message.js';
 
-getData().then((data) => {
-  console.log(data);
-}).catch((error) => {
-  console.log(error);
-});
+
+// getData().then((data) => {
+//   console.log(data);
+// }).catch((error) => {
+//   console.log(error);
+// });
 //генерируем миниатюры с данными юзеров
 //const photos = getAllPhotoUsers();
 renderThumbnails(data);
