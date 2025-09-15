262607b782aa0b9ce686f668adece7dcbcd56c26
AlexKolbe <helping5@yandex.ru>
2025-09-15 14:56:03 +0300

    Смотрим как работает. Пока непонятно

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ import { renderThumbnails } from './thumbnails.js';
 import './form.js';
 import { getData } from './api.js';
 
+getData().then((data) => {
+  console.log(data);
+});
 //генерируем миниатюры с данными юзеров
 const photos = getAllPhotoUsers();
 renderThumbnails(photos);
-getData();
