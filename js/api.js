8147779bdc060b53f8c361176c1e45b69f6fe573
AlexKolbe <helping5@yandex.ru>
2025-09-15 14:25:58 +0300

    Попробывали записать api.js как в уроках

--- js/api.js
+++ js/api.js
@@ -25,3 +25,42 @@
 
  * TODO   Доработайте обработчик закрытия формы, чтобы кроме закрытия формы он сбрасывал введённые пользователем данные и возвращал форму в исходное состояние. Аналогичным образом обработайте нажатие на кнопку сброса.
  */
+
+// Получение данных
+const URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
+
+const Route = {
+  GET: '/data',
+  POST: '',
+};
+
+const Method = {
+  GET: 'GET',
+  POST: 'POST'
+};
+
+const ErrorText = {
+  GET: 'Не удалось загрузить данные. Попробуйте обновить страницу',
+  POST: 'Не удалось отправить форму. Попробуйте еще раз',
+};
+
+const load = async (route, errorText, method = Method.GET, body = null) => {
+  try {
+    const response = await fetch(`${URL}${route}`, {method, body});
+    if (!response.ok) {
+      throw new Error();
+    }
+    return response.json();
+  } catch {
+    throw new Error(errorText);
+  }
+};
+
+const getData = () => load(Route.GET, ErrorText.GET);
+
+const sendData = (body) => load(Route.POST, ErrorText.POST, Method.POST, body);
+
+const data = await getData();
+
+export {getData, sendData, data};
+
