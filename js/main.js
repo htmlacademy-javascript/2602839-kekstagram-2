4f0270b80992030063ce885b3f173318899e1be4
AlexKolbe <helping5@yandex.ru>
2025-09-16 14:43:42 +0300

    убрали лишнее

--- js/main.js
+++ js/main.js
@@ -4,12 +4,4 @@ import './form.js';
 import { data } from './api.js';
 import './form-message.js';
 
-
-// getData().then((data) => {
-//   console.log(data);
-// }).catch((error) => {
-//   console.log(error);
-// });
-//генерируем миниатюры с данными юзеров
-//const photos = getAllPhotoUsers();
 renderThumbnails(data);
