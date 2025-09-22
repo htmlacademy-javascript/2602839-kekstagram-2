ea71f0d05e083051f9c0d1f26a25c51f41d08ad2
Keks <keks@htmlacademy.ru>
2025-09-22 15:28:38 +0300

    :heavy_check_mark: Сборка #16

--- 16/js/main.js
+++ 16/js/main.js
@@ -1,12 +1,27 @@
-//import { getAllPhotoUsers } from './data.js';
 import { renderThumbnails } from './thumbnails.js';
 import './form.js';
-import { data } from './api.js';
+import { getData } from './api.js';
 import './form-message.js';
+import { initFilters, showFilters } from './filter-sorting.js';
 import { showAlert } from './utils.js';
 
-try {
-  renderThumbnails(data);
-} catch {
-  showAlert('Ошибка загрузки изображения');
-}
+// Ожидаем загрузку данных перед рендерингом
+getData()
+  .then((data) => {
+    renderThumbnails(data);
+    initFilters(data);
+    showFilters(); // Показываем фильтры после загрузки данных
+  })
+  .catch((error) => {
+    // Показываем сообщение об ошибке
+    const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
+    const dataErrorElement = dataErrorTemplate.cloneNode(true);
+    document.body.appendChild(dataErrorElement);
+
+    // Убираем сообщение через 5 секунд
+    setTimeout(() => {
+      dataErrorElement.remove();
+    }, 5000);
+
+    showAlert(`Ошибка загрузки данных: ${error}`);
+  });
